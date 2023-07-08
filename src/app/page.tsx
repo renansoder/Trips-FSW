391ad16e4e03fcd9bacf8008ee78ac68d962dd4f
renan.essentjus <renan.essentjus>
2023-07-08 08:14:46 -0300

    feat: teste com next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Image from 'next/image';
+'use client';
+import { signIn, useSession } from 'next-auth/react';
 
 export default function Home() {
-  return <h1>Home Page</h1>;
+  const { data } = useSession();
+
+  return (
+    <div>
+      <button onClick={() => signIn()}>Login</button>
+      <h1>Olá, {data?.user?.name}</h1>
+      <img src={data?.user?.image ?? ''} alt="" />
+    </div>
+  );
 }
