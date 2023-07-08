35705cf99a9bb55d80d39b2056fff08df10fe09f
Renan Soder <renansoder.rs@gmail.com>
2023-07-08 08:33:07 -0300

    feat: testando clonagem repo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-'use client';
-import { signIn, useSession } from 'next-auth/react';
+'use client'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Home() {
-  const { data } = useSession();
+  const { data } = useSession()
 
   return (
     <div>
       <button onClick={() => signIn()}>Login</button>
       <h1>Olá, {data?.user?.name}</h1>
+      <h1>Testando clonagem de repos</h1>
       <img src={data?.user?.image ?? ''} alt="" />
     </div>
-  );
+  )
 }
